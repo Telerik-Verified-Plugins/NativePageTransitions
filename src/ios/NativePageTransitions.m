4dc3a818bc290e90acdba77a0d4ee8d34b2451c6
Peter Staev <peter.staev@gmail.com>
2014-10-30 17:00:00 +0200

    Allow to work with Kendo Adapter and remote views

--- src/ios/NativePageTransitions.m
+++ src/ios/NativePageTransitions.m
@@ -314,7 +314,7 @@
 
 - (BOOL) loadHrefIfPassed:(NSString*) href {
   if (href != [NSNull null]) {
-    if ([href rangeOfString:@".html"].location != NSNotFound) {
+    if (![href hasPrefix:@"#"] && [href rangeOfString:@".html"].location != NSNotFound) {
       // strip any params when looking for the file on the filesystem
       NSString *bareFileName = href;
       NSString *urlParams = nil;
