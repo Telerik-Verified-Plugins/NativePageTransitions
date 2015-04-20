48a6a0c73b7c9910d14e430ce5ce8f65025f0069
Eric Alli <eric@trilabmedia.com>
2015-04-20 00:29:25 -0700

    Add fade transition for iOS

--- src/ios/NativePageTransitions.m
+++ src/ios/NativePageTransitions.m
@@ -462,6 +462,55 @@
   }
 }
 
+- (void) fade:(CDVInvokedUrlCommand*)command {
+    _command = command;
+    NSMutableDictionary *args = [command.arguments objectAtIndex:0];
+    NSTimeInterval duration = [[args objectForKey:@"duration"] doubleValue];
+    NSTimeInterval delay = [[args objectForKey:@"iosdelay"] doubleValue];
+    NSString *href = [args objectForKey:@"href"];
+    
+    // duration is passed in ms, but needs to be in sec here
+    duration = duration / 1000;
+    
+    // overlay the webview with a screenshot to prevent the user from seeing changes in the webview before the fade kicks in
+    CGSize viewSize = self.viewController.view.bounds.size;
+    
+    UIGraphicsBeginImageContextWithOptions(viewSize, YES, 0.0);
+    [self.viewController.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    
+    // Read the UIImage object
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    CGFloat width = self.viewController.view.frame.size.width;
+    CGFloat height = self.viewController.view.frame.size.height;
+    [_screenShotImageView setFrame:CGRectMake(0, 0, width, height)];
+    
+    _screenShotImageView = [[UIImageView alloc]initWithFrame:[self.viewController.view.window frame]];
+    [_screenShotImageView setImage:image];
+    [self.transitionView.superview insertSubview:_screenShotImageView aboveSubview:self.transitionView];
+    
+    UIViewAnimationOptions animationOptions;
+    animationOptions = UIViewAnimationOptionTransitionCrossDissolve;
+    
+    if ([self loadHrefIfPassed:href]) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
+            // remove the screenshot halfway during the transition
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (duration/2) * NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
+                [_screenShotImageView removeFromSuperview];
+            });
+            [UIView transitionWithView:self.viewController.view
+                              duration:duration
+                               options:animationOptions | UIViewAnimationOptionAllowAnimatedContent
+                            animations:^{}
+                            completion:^(BOOL finished) {
+                                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                            }];
+        });
+    }
+}
+
 - (BOOL) loadHrefIfPassed:(NSString*) href {
   if (href != nil && ![href isEqual:[NSNull null]]) {
     if (![href hasPrefix:@"#"] && [href rangeOfString:@".html"].location != NSNotFound) {
